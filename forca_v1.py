4d53a5243fbceb6d0b68847f0b2cb9a35fd40b88
Cintia <cintia@ago.digital>
2019-04-26 17:38:32 -0300

    2º commit

--- forca_v1.py
+++ forca_v1.py
@@ -69,3 +69,94 @@ board = ['''
 / \  |
      |
 =========''']
+
+#Classe
+class Forca:
+
+    #construtor
+    def __init__(self,palavra,status):
+        self.palavra = palavra
+        self.status = status
+
+    #adivinhar a letra
+    def adivinhar(self, letra):
+        pos=[]
+        cont = self.palavra.count(letra)
+        x = 0
+        while x < cont:
+            y = 0
+            while y < len(self.palavra):
+                if self.palavra[y] == letra:
+                    pos.append(y)
+                    x += 1
+                y += 1
+        return pos
+        #self.palavra.find(letra)
+        #return self.palavra.count(letra)
+
+    #verificar se o jogo terminou
+    def terminou_forca(self, palavra):
+        return self.status == 7 or self.palavra == palavra
+
+    #verificar se o jogador venceu
+    def venceu_forca(self, palavra):
+        return self.palavra == palavra
+
+    #checar o status do game e printar o board na tela
+    def print_status_jogo(self):
+        #print(board[self.status])
+        return board[self.status]
+
+#lê uma palavra aletória do arquivo
+def rand_palavra():
+    with open("palavras.txt","rt") as f:
+        bank = f.readlines()
+    return bank[random.randint(0,len(bank)-1)].strip()
+
+#main
+def main():
+    #objeto
+    game = Forca(rand_palavra(),0)
+
+    #status = 0
+    letrasErradas = ''
+    letrasCorretas = ''
+    palavraAux = []
+    for x in game.palavra:
+        palavraAux.append('_')
+    teste = '_'*len(game.palavra)
+    #enquanto o jogo não tiver terminado, print do status, solicita uma letra e faz a leitura do caracter
+    while not game.terminou_forca(teste):
+        print(game.print_status_jogo())
+        print('\nPalavra: '+teste)
+        #print(palavraAux)
+        print('\nLetras Erradas: \n' + letrasErradas)
+        print('\nLetras Corretas: \n' + letrasCorretas)
+        letra = input('Digite uma letra: ')
+        pos = game.adivinhar(letra)
+        if len(pos) > 0:
+            letrasCorretas += letra
+            for x in pos:
+                palavraAux[x] = letra
+        else:
+            letrasErradas += letra
+            game.status += 1
+        teste = ''
+        for x in palavraAux:
+            teste += x
+
+    #verifica o status do jogo
+    #game.print_status_jogo()
+
+    #de acordo com o status, imprime mensagem na tela para o usuário
+    if game.venceu_forca(teste):
+        print('\nParabéns! Você venceu!')
+    else:
+        print('\nGame over! Você perdeu.')
+        print('A palavra era '+game.palavra)
+
+    print('\nFoi bom jogar com você! Agora vá estudar!\n')
+
+#executa o programa
+if __name__ == "__main__":
+    main()
--- forca_v1_t.py
+++ forca_v1_t.py
@@ -70,28 +70,53 @@ O   |
 class Hangman:
 
 	# Método Construtor
-	def __init__(self, word):
-		
+	def __init__(self, palavra):
+		self.palavra = palavra
+		self.letras_erradas = []
+		self.letras_corretas = []
 		
 	# Método para adivinhar a letra
-	def guess(self, letter):
-		
+	def adivinhar(self, letra):
+		if letra in self.palavra and letra not in self.letras_corretas:
+			self.letras_corretas.append(letra)
+		elif letra not in self.palavra and letra not in self.letras_erradas:
+			self.letras_erradas.append(letra)
+		else:
+			return False
+		return True
 		
 	# Método para verificar se o jogo terminou
-	def hangman_over(self):
-		
+	def terminou_jogo(self):
+		return self.venceu_jogo() or (len(self.letras_erradas == 6))
 		
 	# Método para verificar se o jogador venceu
-	def hangman_won(self):
-		
+	def venceu_jogo(self):
+		if '_'not in self.palavra_escondida():
+			return True
+		return False
 
 	# Método para não mostrar a letra no board
-	def hide_word(self):
-		
+	def palavra_escondida(self):
+		rtn = ''
+		for letra in self.palavra:
+			if letra not in self.letras_corretas:
+				rtn += '_'
+			else:
+				rtn += letra
+		return  rtn
 		
 	# Método para checar o status do game e imprimir o board na tela
-	def print_game_status(self):
-		
+	def print_status_jogo(self):
+		print(board[len(self.letras_erradas)])
+		print('\nPalavra: '+self.palavra_escondida())
+		print('\nLetras Erradas:')
+		for letra in self.letras_erradas:
+			print(letra,)
+		print()
+		print('\nLetras corretas:')
+		for letra in self.letras_corretas:
+			print(letra,)
+		print()
 
 # Função para ler uma palavra de forma aleatória do banco de palavras
 def rand_word():
